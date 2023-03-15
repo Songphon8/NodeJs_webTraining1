60d9ed58f83e12fbdbb63abac3b1aba950bee0e7
Songphon8 <songphon.kas@gmail.com>
2023-03-15 22:58:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const{ res } = require('express');
 const morgan = require('morgan');
 const app = express();
-const port = process.env.port
+const port = process.env.port|| 3030;
 
 app.use(express.static('./public'));
 app.use(morgan("common",{immediate:true}));
